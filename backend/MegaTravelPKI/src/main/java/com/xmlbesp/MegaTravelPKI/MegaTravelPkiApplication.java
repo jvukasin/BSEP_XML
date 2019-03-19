68fbe5fc79fc3a14dbd375a53d2475e564230d09
Milan Lazic <lazicy@gmail.com>
2019-03-19 23:37:15 +0100

    pocetak avanture

---
+++ backend/MegaTravelPKI/src/main/java/com/xmlbesp/MegaTravelPKI/MegaTravelPkiApplication.java
@@ -0,0 +1,14 @@
+package com.xmlbesp.MegaTravelPKI;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MegaTravelPkiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MegaTravelPkiApplication.class, args);
+		
+	}
+
+}
