fb59088523e38841673dfc79afd237c3362e4ae5
Milan Lazic <lazicy@gmail.com>
2019-03-21 18:54:00 +0100

    cors

--- backend/MegaTravelPKI/src/main/java/com/xmlbesp/MegaTravelPKI/MegaTravelPkiApplication.java
+++ backend/MegaTravelPKI/src/main/java/com/xmlbesp/MegaTravelPKI/MegaTravelPkiApplication.java
@@ -2,6 +2,8 @@
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class MegaTravelPkiApplication {
@@ -10,5 +12,16 @@ public class MegaTravelPkiApplication {
 		SpringApplication.run(MegaTravelPkiApplication.class, args);
 		
 	}
+	
+	
+	@Bean
+    public FilterRegistrationBean corsFilterRegistration() {
+        FilterRegistrationBean registrationBean =
+                new FilterRegistrationBean(new CORSFilter());
+        registrationBean.setName("CORS Filter");
+        registrationBean.addUrlPatterns("/*");
+        registrationBean.setOrder(1);
+        return registrationBean;
+    }
 
 }
