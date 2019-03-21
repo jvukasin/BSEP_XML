d434036456a85c0d144398eba52fdff43a1d14b5
Vukasin <jovic.vukasin@gmail.com>
2019-03-21 12:51:24 +0100

    stating work for CP1

---
+++ frontend/mega-travel-app/src/app/pki/pki.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-pki',
+  templateUrl: './pki.component.html',
+  styleUrls: ['./pki.component.css']
+})
+export class PKIComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
