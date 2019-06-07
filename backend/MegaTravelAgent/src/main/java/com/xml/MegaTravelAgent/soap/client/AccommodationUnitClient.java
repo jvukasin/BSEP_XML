3b20aa54368c7377fc6fbeb42de0df8f81718d03
Vladimir Cvetanovic <cvetanovic9696@gmail.com>
2019-06-07 19:03:29 +0200

    zapocet consumer za soap

---
+++ backend/MegaTravelAgent/src/main/java/com/xml/MegaTravelAgent/soap/client/AccommodationUnitClient.java
@@ -0,0 +1,8 @@
+package com.xml.MegaTravelAgent.soap.client;
+
+import org.springframework.ws.client.core.support.WebServiceGatewaySupport;
+
+
+public class AccommodationUnitClient extends WebServiceGatewaySupport {
+
+}
