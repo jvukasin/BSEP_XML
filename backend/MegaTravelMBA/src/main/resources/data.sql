d35e63360d72c160b42aa76fa8f6fcbffb2cd91a
Vukasin <jovic.vukasin@gmail.com>
2019-06-07 14:36:37 +0200

    still testing soap...

--- backend/MegaTravelMBA/src/main/resources/data.sql
+++ backend/MegaTravelMBA/src/main/resources/data.sql
@@ -11,3 +11,11 @@ insert into users_roles values ('dovla',1);
 insert into users_roles values ('laza',2);
 
 insert into roles_privileges values (1,1);
+
+INSERT INTO `megatravel_mba`.`accommodation_unit` (`id`, `cancellation_period`, `capacity`, `default_price`, `description`, `name`, `price`, `rating_avg`, `type`, `location_id`) VALUES ('1', '3', '3', '34', 'sdf', 'ASA', '36', '6', 'hotel', '1');
+
+INSERT INTO `megatravel_mba`.`location` (`id`, `coordinates`, `accommodation_unit_id`, `city_id`) VALUES ('1', '234234', '1', '1');
+
+INSERT INTO `megatravel_mba`.`city` (`id`, `name`, `country_id`, `location_id`) VALUES ('1', 'Novi Sad', '1', '1');
+
+INSERT INTO `megatravel_mba`.`country` (`id`, `name`) VALUES ('1', 'Srbija');
