6b2193159edc0ced359c12b3bb8480ccb2142361
Vukasin <jovic.vukasin@gmail.com>
2019-06-21 10:48:07 +0200

    reservation

---
+++ frontend/mega-travel/src/app/services/reservation.service.ts
@@ -0,0 +1,15 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from "@angular/core";
+import { Router } from '@angular/router';
+import { ReturnStatement } from '@angular/compiler';
+
+
+@Injectable()
+export class ReservationService {
+
+    constructor(private http: HttpClient, private router: Router) { }
+
+    makeReservation(res) {
+        return this.http.post("/api/reservationservice/reservations", res);
+    }
+}
--- frontend/mega-travel/src/app/services/user.service.ts
+++ frontend/mega-travel/src/app/services/user.service.ts
@@ -7,6 +7,10 @@ export class UserService {
 
     constructor(private http: HttpClient) { }
 
+    getUser() {
+        return this.http.get("/api/authservice/users/getUser");
+    }
+
     registerUser(user) {
         return this.http.post("/api/authservice/users", user);
     }
