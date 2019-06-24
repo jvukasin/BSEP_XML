35406a944f8530c1ec4516ce3ec74b7b82304ee4
Vukasin <jovic.vukasin@gmail.com>
2019-06-24 12:40:53 +0200

    changes

--- backend/accommodation-service/src/main/resources/data.sql
+++ backend/accommodation-service/src/main/resources/data.sql
@@ -160,7 +160,7 @@ INSERT INTO location (id, coordinates, distance_from_city, city_id) values
 
 INSERT INTO accommodation_unit (id, cancellation_period, capacity, category, default_price, description, name, price, rating_avg, type, agent_username, location_id) values
 
-(1, 15, 2, 5, 220, 'Lovely apartment near the city center hosted by BBF Enterprise company. Has a gym and a shopping center near by to add to your everyday outdoor routine.', 'Apartment BBF', 220, 9.6, 'apartment', 'tjokarda', 1),
+(1, 15, 4, 5, 220, 'Lovely apartment near the city center hosted by BBF Enterprise company. Has a gym and a shopping center near by to add to your everyday outdoor routine.', 'Apartment BBF', 220, 9.6, 'apartment', 'tjokarda', 1),
 (2, 20, 2, 3, 118, 'Has a gym and a shopping center near by to add to your everyday outdoor routine.', 'Hostel room', 118, 8.4, 'hostel', 'tjokarda', 4),
 (3,10,2,-1,60,'Very nice apartment near city center very close to train station and main tourist attractions. Ideal option for young people who wants to spend great time in Madrid!','Chilare classico',0,0,'studio','laza',5),
 (4, 18, 4, 3, 176, 'Very nice apartment near city center very close to train station and main tourist attractions. Ideal option for young people who want to spend great time in Novi Sad!', 'Hotel Gajic', 176, 9.0, 'Hotel', 'tjokarda', 6);
@@ -191,10 +191,6 @@ INSERT INTO amenity (id, fa_icon, name) values
 (10, 'fa fa-paw', 'Pet friendly'),
 (11, 'fa fa-shower', 'Private bathroom'),
 (12, 'fa fa-cutlery', 'Kitchen');
-
-
-insert into reservation (id, start_date, end_date, price, accommodation_unit_id, is_successful, reservator_username)
-values (1, '2019-06-25', '2019-06-27', '500', 1, false, 'laza');
 
 insert into accommodationunit_amenity (amenity_id, accommodation_unit_id) values
 (1, 1), (2, 1), (3, 1), (5, 1), (4, 1), (12, 1), (4, 2), (6, 2), (10, 2);
