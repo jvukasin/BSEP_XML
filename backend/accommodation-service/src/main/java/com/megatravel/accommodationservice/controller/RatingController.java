79ab8e4f6fa887ea86e1b9367d338dc07ebf2d31
Vladimir Cvetanovic <cvetanovic9696@gmail.com>
2019-06-29 18:48:34 +0200

    Dodat kontroler za Rating

---
+++ backend/accommodation-service/src/main/java/com/megatravel/accommodationservice/controller/RatingController.java
@@ -0,0 +1,10 @@
+package com.megatravel.accommodationservice.controller;
+
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(value = "/ratings")
+public class RatingController {
+}
