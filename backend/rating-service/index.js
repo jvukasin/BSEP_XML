f75281d5d0711188baa13022cd3567b373d61553
Vukasin <jovic.vukasin@gmail.com>
2019-06-29 18:59:12 +0200

    dodavanje rejtinga

--- backend/rating-service/index.js
+++ backend/rating-service/index.js
@@ -1,5 +1,20 @@
 const connection = require('./database')
 
+exports.addRating = function addRating(req, res) {
+	let value = req.body.value;
+	let approved = req.body.approved;
+	let posting_date = req.body.posting_date;
+	let comment = req.body.comment;
+	let accommodation_id = req.body.accommodation_id;
+	let reservator = req.body.reservator;
+	connection.query("insert into rating (value, approved, posting_date, comment, accommodation_id, reservator) values (?,?,?,?,?,?)",[value, approved, posting_date, comment, accommodation_id, reservator], (err, result) => {
+		if (err) res.status(400).send(err);
+		else {
+			res.status(200).send('added');
+		}
+	}
+}
+
 exports.getAllRatings = function getAllRatings(req, res) {
     connection.query("select * from rating", (err, result)=> {
 	if (err) res.status(400).send(err);
